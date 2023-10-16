7caf2eaa46afd5b28aa61e3a3d29787e10f5f2aa
matheus.msouza26 <matheus.msouza26@SBC0588965W10-1>
2023-10-16 12:02:56 -0300

    Adicionar arquivos de projeto.

---
+++ App01.Aula01/View/TelaPrincipal.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace App01.Aula01.View
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.lbl_Nome = new System.Windows.Forms.Label();
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.Txb_Nome = new System.Windows.Forms.TextBox();
+            this.Txb_Email = new System.Windows.Forms.TextBox();
+            this.lbl_Resultado = new System.Windows.Forms.Label();
+            this.btn_Salvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.BackColor = System.Drawing.Color.Red;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Titulo.Location = new System.Drawing.Point(24, 64);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(390, 40);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Cadastro de Registro";
+            // 
+            // lbl_Nome
+            // 
+            this.lbl_Nome.AutoSize = true;
+            this.lbl_Nome.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Nome.Location = new System.Drawing.Point(18, 127);
+            this.lbl_Nome.Name = "lbl_Nome";
+            this.lbl_Nome.Size = new System.Drawing.Size(72, 25);
+            this.lbl_Nome.TabIndex = 1;
+            this.lbl_Nome.Text = "Nome :";
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Email.Location = new System.Drawing.Point(15, 165);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(75, 25);
+            this.lbl_Email.TabIndex = 2;
+            this.lbl_Email.Text = "E-mail :";
+            // 
+            // Txb_Nome
+            // 
+            this.Txb_Nome.Location = new System.Drawing.Point(96, 127);
+            this.Txb_Nome.Name = "Txb_Nome";
+            this.Txb_Nome.Size = new System.Drawing.Size(332, 23);
+            this.Txb_Nome.TabIndex = 3;
+            // 
+            // Txb_Email
+            // 
+            this.Txb_Email.Location = new System.Drawing.Point(96, 165);
+            this.Txb_Email.Name = "Txb_Email";
+            this.Txb_Email.Size = new System.Drawing.Size(332, 23);
+            this.Txb_Email.TabIndex = 4;
+            // 
+            // lbl_Resultado
+            // 
+            this.lbl_Resultado.BackColor = System.Drawing.Color.White;
+            this.lbl_Resultado.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Resultado.Location = new System.Drawing.Point(15, 220);
+            this.lbl_Resultado.Name = "lbl_Resultado";
+            this.lbl_Resultado.Size = new System.Drawing.Size(408, 122);
+            this.lbl_Resultado.TabIndex = 5;
+            this.lbl_Resultado.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btn_Salvar
+            // 
+            this.btn_Salvar.BackColor = System.Drawing.Color.White;
+            this.btn_Salvar.Location = new System.Drawing.Point(15, 365);
+            this.btn_Salvar.Name = "btn_Salvar";
+            this.btn_Salvar.Size = new System.Drawing.Size(408, 45);
+            this.btn_Salvar.TabIndex = 6;
+            this.btn_Salvar.Text = "Salvar";
+            this.btn_Salvar.UseVisualStyleBackColor = false;
+            this.btn_Salvar.Click += new System.EventHandler(this.btn_Salvar_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(435, 623);
+            this.Controls.Add(this.btn_Salvar);
+            this.Controls.Add(this.lbl_Resultado);
+            this.Controls.Add(this.Txb_Email);
+            this.Controls.Add(this.Txb_Nome);
+            this.Controls.Add(this.lbl_Email);
+            this.Controls.Add(this.lbl_Nome);
+            this.Controls.Add(this.lbl_Titulo);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "TelaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Texto";
+            this.Load += new System.EventHandler(this.TelaPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_Titulo;
+        private Label lbl_Nome;
+        private Label lbl_Email;
+        private TextBox Txb_Nome;
+        private TextBox Txb_Email;
+        private Label lbl_Resultado;
+        private Button btn_Salvar;
+    }
+}
---
+++ App01.Aula01/View/TelaPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App01.Aula01.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TelaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            String nome = Txb_Nome.Text;
+            String email = Txb_Email.Text;
+
+            lbl_Resultado.Text = "\nSeu nome é " + nome + "\nE seu Email é " + email +"\nBom te ver aqui!!";
+        }
+    }
+}
